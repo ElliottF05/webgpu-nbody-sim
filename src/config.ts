970f89b51e490b63981e3808397b6ce2e0650389
ElliottF05 <elliotthfaa@gmail.com>
2025-12-23 15:40:03 -0500

    feat: increase number of points to 100k

--- src/config.ts
+++ src/config.ts
@@ -9,10 +9,10 @@ export type SimConfig = Readonly<{
 }>;
 
 export function getDefaultSimConfig(canvas: HTMLCanvasElement): SimConfig {
-    const numBodies = 8000;
+    const numBodies = 100000;
     const gravConstant = 1.0;
     const bhTheta = 0.5;
-    const substeps = 10;
+    const substeps = 1;
     const viewPort: [number, number] = [canvas.width, canvas.height];
     const deltaTime = 0.1 * 1.0 / (60.0 * substeps);
     const epsilonMultiplier = 1.0;
