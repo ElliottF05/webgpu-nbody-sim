5891c826d15c1b6dd03e9d82526b96e0cc357903
ElliottF05 <elliotthfaa@gmail.com>
2025-12-23 20:18:15 -0500

    feat: limit to 30fps

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { InteractionController } from "./interaction";
 import { Renderer } from "./renderer";
 import { Simulation } from "./simulation";
 
+const TARGET_FPS = 30;
+const FRAME_DURATION_MS = 1000.0 / TARGET_FPS;
+
 export interface GPUCommandSource {
     getCommands(): GPUCommandBuffer;
 }
@@ -15,14 +18,23 @@ async function main() {
     const renderer = new Renderer(device, context, sim.getConfig(),sim.getBuffers(), canvasFormat);
     const interaction = new InteractionController(device, canvasFormat, canvas, context, sim);
 
+    let last = performance.now();
     function frame() {
+        requestAnimationFrame(frame);
+
+        const now = performance.now();
+        if (now - last < FRAME_DURATION_MS) {
+            return;
+        }
+        last = now;
+
+        interaction.sendUpdateToGPU();
         device.queue.submit([
             sim.getCommands(),
             renderer.getCommands(),
         ]);
-        interaction.sendUpdateToGPU();
-        requestAnimationFrame(frame);   
     }
+    
     requestAnimationFrame(frame);
 }
 
